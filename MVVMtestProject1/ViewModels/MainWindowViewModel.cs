38afb126c1f2d3173dd455447e02646a927c3645
Karachev, Mikhail A <obito_uchiha@inbox.ru>
2019-10-05 14:50:37 +0500

    for Danya

---
+++ MVVMtestProject1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using Avalonia;
+
+namespace MVVMtestProject1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string greeting;
+
+        public string Greeting
+        {
+            get => greeting;
+            set => greeting = value;
+        }
+        public ReactiveCommand<Unit, Unit> WhenChangedCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Greeting = "Welcome to Avalonia!";
+            WhenChangedCommand = ReactiveCommand.Create(() => { Console.WriteLine(Greeting); Greeting += "!"; });
+        }
+    }
+}
---
+++ MVVMtestProject1/Views/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using MVVMtestProject1.ViewModels;
+using ReactiveUI;
+using System.Reactive.Disposables;
+
+namespace MVVMtestProject1.Views
+{
+    public class MainWindow : ReactiveWindow<MainWindowViewModel>
+    {
+        public TextBox tb1 => this.FindControl<TextBox>("tb1");
+        public Button btn1 => this.FindControl<Button>("btn1");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            this.WhenActivated(disposables =>
+            {
+                this.Bind(ViewModel, vm => vm.Greeting, v => v.tb1.Text).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.WhenChangedCommand, v => v.btn1).DisposeWith(disposables);
+            });
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
